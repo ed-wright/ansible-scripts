d79b8c0ca763c237978473f5939eaccfb4ea3e5b
Edward Wright <ed.wright@live.co.uk>
2018-08-18 15:00:26 +0100

    Update menu.sh

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 
 # Check the script is running as root
 if [ "$EUID" -ne 0 ]; then
@@ -7,9 +8,25 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Check this script is up to date
-	
-	curl -sSL https://raw.githubusercontent.com/ed-wright/ansible-scripts/master/version.txt
-	cat /opt/ansible-scripts/version.txt
+varA=$(curl -sSL https://raw.githubusercontent.com/ed-wright/ansible-scripts/master/version.txt)
+varB=$(cat /opt/ansible-scripts/version.txt)
+
+if  [ "$varA" != "$varB" ]; then
+		#printf "You version is out of date please run the following command:\n"
+		#printf '\033[32msudo git reset --hard origin/master\033[0m\n';
+		#exit 1;
+		
+		printf "Your script is out of date, updating..."
+		#remove any old versions of the update script
+		sudo rm -rf /tmp/ansible-scripts
+		#make temp directory
+		sudo mkdir /tmp/ansible-scripts
+		#copy the update script to the tmp dir
+		cp /opt/ansible-scripts/scripts/update.sh /tmp/ansible-scripts/update.sh
+		bin/bash /tmp/ansible-scripts/update.sh
+fi
+	#curl -sSL https://raw.githubusercontent.com/ed-wright/ansible-scripts/master/version.txt
+	#cat /opt/ansible-scripts/version.txt
 	
 # Show user selection of scripts
 # Execute selection
