bedc63d4ad481e3e6cbcf69a444b5c2c6d257277
Edward Wright <ed.wright@live.co.uk>
2018-08-18 12:39:28 +0100

    Update menu.sh

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -36,3 +36,8 @@ case $CHOICE in
             echo "You chose Option 3"
             ;;
 esac
+
+dialog --clear --backtitle "Backtitle here" --title "Title here" --menu "Choose one of the following options:" 15 40 4 \
+1 "Option 1" \
+2 "Option 2" \
+3 "Option 3"
