93ad123c5e649d219a31ff5b704fe96d5ae9fff0
Edward Wright <ed.wright@live.co.uk>
2018-08-19 10:11:42 +0100

    Update menu.sh

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -8,14 +8,11 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Check this script is up to date
-varA=$(curl -sSL https://raw.githubusercontent.com/ed-wright/ansible-scripts/master/version.txt)
-varB=$(cat /opt/ansible-scripts/version.txt)
+sudo git fetch
+varA=$(git rev-parse HEAD)
+varB=$(git rev-parse @{u})
 
 if  [ "$varA" != "$varB" ]; then
-		#printf "You version is out of date please run the following command:\n"
-		#printf '\033[32msudo git reset --hard origin/master\033[0m\n';
-		#exit 1;
-		
 		printf "Your script is out of date, updating..."
 		#remove any old versions of the update script
 		sudo rm -rf /tmp/ansible-scripts
@@ -30,23 +27,16 @@ fi
 	
 # Show user selection of scripts
 # Execute selection
-
 PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
+options=("Install Ansible Host" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
+        "Install Ansible Host")
+			/bin/bash /opt/ansible-scripts/scripts/ansible-host-init.sh
             ;;
         "Quit")
-            break
+            exit 0
             ;;
         *) echo "invalid option $REPLY";;
     esac
