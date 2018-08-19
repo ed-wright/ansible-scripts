d54b62cafff9c7577d9e1ef05a6d862e966a21e7
Edward Wright <ed.wright@live.co.uk>
2018-08-19 22:32:25 +0100

    Update minimal.sh

--- scripts/minimal.sh
+++ scripts/minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #####################
-	# Command Line Help #
+# Command Line Help #
 #####################
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
@@ -19,11 +19,11 @@ printf "*********************************\n"
 printf "*        Minimal Install        *\n"
 printf "*********************************\n"
 
-printf "\nTASK: [Update Host]\n"
+printf "\n\033[32mTASK: [Update Host]\n"
 # Check the host is up to date
 sudo yum update -y
 
-printf "\nTASK: [Install Git]\n"
+printf "\n\033[32mTASK: [Install Git]\033[0m\n"
 # check if git is installed, if it is install otherwise skip
 if command -v git != "0" ; then
 	printf "git already installed skipping"	
@@ -31,13 +31,14 @@ else
 	sudo yum install git -y
 fi
 
-printf "\nTASK: [Prepare Directories]\n"
+printf "\n\033[32mTASK: [Prepare Directories]\033[0m\n"
 # Remove old versions of scripts
 sudo rm -rf /opt/ansible-scripts/
 # Change directory to /opt
 cd /opt || exit 1
 
-printf "\nTASK: [Fetch Latest Version of the Repo]\n"
+printf "\n\033[32mTASK: [Fetch Latest Version of the Repo]\033[0m\n"
 sudo git clone https://github.com/ed-wright/ansible-scripts.git
 printf -- '\033[32mDONE \033[0m\n';
+/bin/bash /opt/ansible-scripts/scripts/menu.sh
 exit 0;
